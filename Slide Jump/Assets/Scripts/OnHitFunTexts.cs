06a5a8a8267612962df369fadc8bd6ce0ed01a49
Kenan <kenan.binay@hotmail.com>
2021-11-28 23:44:58 +0300

    OnHit active random text

---
+++ Slide Jump/Assets/Scripts/OnHitFunTexts.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnHitFunTexts : MonoBehaviour
+{
+    public GameObject text1, text2, text3, text4, text5,spawnPoint;
+    float randX;
+    private int[] values = { 1, 2, 3, 4, 5 };
+    int nextText; 
+    Vector3 whereToSpawn;
+    public static float SpeedText = 1f;
+
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+     
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Spike"))
+        {
+            nextText = values[Random.Range(0, values.Length)];
+            randX = Random.Range(-3.2f,4f);
+            whereToSpawn = new Vector3(randX, spawnPoint.transform.position.y, spawnPoint.transform.position.z);
+
+            Debug.Log("Text = " + nextText);
+            switch (nextText)
+            {
+                case 1:
+                    Debug.Log("text1 spawned");
+                    Instantiate(text1, whereToSpawn, Quaternion.identity);
+                    StartCoroutine(endText());
+                    break;
+
+                case 2:
+                    Debug.Log("text2 spawned");
+                    Instantiate(text2, whereToSpawn, Quaternion.identity);
+                    StartCoroutine(endText());
+                    break;
+
+                case 3:
+                    Debug.Log("text3 spawned");
+                    Instantiate(text3, whereToSpawn, Quaternion.identity);
+                    StartCoroutine(endText());
+                    break;
+
+                case 4:
+                    Debug.Log("text4 spawned");
+                    Instantiate(text4, whereToSpawn, Quaternion.identity);
+                    StartCoroutine(endText());
+                    break;
+
+                case 5:
+                    Debug.Log("text5 spawned");
+                    Instantiate(text5, whereToSpawn, Quaternion.identity);
+                    StartCoroutine(endText());
+                    break;
+            }
+               
+        }
+    }
+
+    private IEnumerator endText()
+    {
+        yield return new WaitForSeconds(1f);
+
+            Debug.Log("Texts Refreshed");
+            Destroy(text1);
+            Destroy(text2);
+            Destroy(text3);
+            Destroy(text4);
+            Destroy(text5);
+        
+    }
+}
