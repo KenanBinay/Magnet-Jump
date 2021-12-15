4a36d531172e4c5f49e8486d75f6f8c97fb539ad
Kenan <kenan.binay@hotmail.com>
2021-12-15 18:17:33 +0300

    Fixing menu

--- Slide Jump/Assets/Scripts/HearthLogoAnimControll.cs
+++ Slide Jump/Assets/Scripts/HearthLogoAnimControll.cs
@@ -13,34 +13,36 @@ public class HearthLogoAnimControll : MonoBehaviour
     
     void FixedUpdate()
     {
-        if (Pause.controlPaused == false)
+        if (MenuPlayGame.MenuStart == false)
         {
-            if (GameOverScreen.GameEnd == false)
+            if (Pause.controlPaused == false)
             {
-                if (Player.SpikeControl == false)
+                if (GameOverScreen.GameEnd == false)
                 {
+                    if (Player.SpikeControl == false)
+                    {
 
-                    if (decrease == false)
-                    {
-                        FinalHeigh = 0.22f;
-                        Speed = 1f;
-                        float newScale = Mathf.Lerp(SeedHeight, FinalHeigh, Time.time * Speed);
-                        transform.localScale = new Vector3(newScale, newScale, newScale);
-                        StartCoroutine(HearthIncrease());
-                    }
-                    if (decrease)
-                    {
-                        FinalHeigh = 0.20f;
-                        Speed = 1f;
-                        float newScale = Mathf.Lerp(SeedHeight, FinalHeigh, Time.time * Speed);
-                        transform.localScale = new Vector3(newScale, newScale, newScale);
-                        if (transform.localScale == new Vector3(0.20f, 0.20f, 0.20f))
-                        { decrease = false; }
+                        if (decrease == false)
+                        {
+                            FinalHeigh = 0.22f;
+                            Speed = 1f;
+                            float newScale = Mathf.Lerp(SeedHeight, FinalHeigh, Time.time * Speed);
+                            transform.localScale = new Vector3(newScale, newScale, newScale);
+                            StartCoroutine(HearthIncrease());
+                        }
+                        if (decrease)
+                        {
+                            FinalHeigh = 0.20f;
+                            Speed = 1f;
+                            float newScale = Mathf.Lerp(SeedHeight, FinalHeigh, Time.time * Speed);
+                            transform.localScale = new Vector3(newScale, newScale, newScale);
+                            if (transform.localScale == new Vector3(0.20f, 0.20f, 0.20f))
+                            { decrease = false; }
+                        }
                     }
                 }
             }
         }
-
         if (Player.SpikeControl)
         {
             FinalHeigh = 0.15f;
